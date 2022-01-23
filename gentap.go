c7d7e87e281f580089b011c1cecf0e153281eb1a
ysicing <i@ysicing.me>
2022-01-23 14:31:37 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- gentap.go
+++ gentap.go
@@ -6,16 +6,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"strings"
+
 	"github.com/ergoapi/exgin"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/webhooks/v6/github"
 	"github.com/ysicing/ergo/pkg/util/ssh"
-	"log"
 )
 
 var port int
 
-func main()  {
+func main() {
 	flag.IntVar(&port, "p", 8080, "port")
 	flag.Parse()
 	hook, _ := github.New(github.Options.Secret(""))
@@ -28,11 +30,22 @@ func main()  {
 		}
 		switch payload.(type) {
 		case github.ReleasePayload:
-			log.Println("收到release hook")
-			err := ssh.RunCmd("/bin/bash", "/root/homebrew-tap/genergo.sh")
-			if err != nil {
-				exgin.GinsData(c, nil, err)
-				return
+			release := payload.(github.ReleasePayload)
+			fullname := release.Repository.FullName
+			v := release.Release.TagName
+			log.Println("收到 ", fullname, " release: ", v)
+			if strings.Contains(fullname, "ergo") {
+				err := ssh.RunCmd("/bin/bash", "/root/homebrew-tap/genergo.sh")
+				if err != nil {
+					exgin.GinsData(c, nil, err)
+					return
+				}
+			} else if strings.Contains(fullname, "kube-resource") {
+				err := ssh.RunCmd("/bin/bash", "/root/homebrew-tap/kr.sh")
+				if err != nil {
+					exgin.GinsData(c, nil, err)
+					return
+				}
 			}
 		}
 		exgin.GinsData(c, nil, nil)
